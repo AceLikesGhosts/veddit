ffb7326e0c1f3695bad01a5087a3063d1d849664
AceLikesGhosts <acelikesghosts@gmail.com>
2023-04-28 16:41:28 -0700

    feat\!(about): add ability to get subreddit's about.json AND changed name scheme of user about

--- veddit/about.v
+++ veddit/about.v
@@ -3,8 +3,141 @@ module veddit
 import net.http { get }
 import json
 
-// user fetches a user from reddit, it pulls their /about.json and parses it into a V struct
-pub fn user(name string) !User {
+pub fn about_subreddit(name string) !AboutSubreddit {
+	mut resp := get('https://reddit.com/r/${name}/about.json') or {
+		return error('Failed to get subreddit
+		(${name})\'s information (https://reddit.com/r/${name}/about.json).
+		\nOriginal ${err}')
+	}
+
+	if resp.status_code == 404 {
+		return error('Failed to find subreddit (${name})\'s information (https://reddit.com/r/${name}/about.json)')
+	}
+
+	return json.decode(AboutSubreddit, resp.body)
+}
+
+struct AboutSubreddit {
+pub:
+	kind string
+	data AboutSubredditData
+}
+
+struct AboutSubredditData {
+pub:
+	accept_followers bool
+	accounts_active int
+	accounts_active_is_fuzzed bool
+	active_user_count bool
+	advertiser_category string
+	all_original_content bool
+	allow_chat_post_creation bool
+	allow_discovery bool
+	allow_galleries bool
+	allow_iamges bool
+	allow_polls bool
+	allow_prediction_contributors bool
+allow_predictions_tournament bool
+	allow_talks bool
+	allow_videogifs bool
+	allow_videos bool
+	allow_media_in_comments []string
+	banner_background_color string
+	banner_background_image string
+	banner_img string
+	banner_size []int
+	can_assign_link_flair bool
+	can_assign_user_flair bool
+	collapse_deleted_comments bool
+	comment_contribution_settings  Subreddit_CommentContributionSettings
+	comment_score_hide_mins int
+	community_icon string
+community_reviewed bool
+	content_category string
+	created int
+	created_utc int
+	description string
+description_html string
+	disable_contributor_requests bool
+	display_name string
+	display_name_prefixed string
+	emojis_custom_size []int // TODO: maybe comment out cause no subreddits ive tested have this actually
+	emojis_enabled bool
+	free_form_reports bool
+	has_menu_widget bool
+	header_img string
+	header_size []int
+	header_title string
+	hide_ads bool
+	icon_img string
+	icon_size []int
+	id string
+	is_chat_post_feature_enabled bool
+	is_crosspostable_subreddit bool
+	is_enrolled_in_new_modmail bool //nullable
+	key_color string
+	lang string
+	link_flair_enabled bool
+	link_flair_position string
+	mobile_banner_iamge string
+	name string
+	// notification_level string -> TODO: no subreddits ive tested have this set to a value
+	original_content_tag_enabled bool
+	over18 bool
+	prediction_leaderboard_entry_type string
+	primary_color string
+	public_description string
+public_description_html string
+public_traffic bool
+	quarantine bool
+	restrict_commenting bool
+	restrict_posting bool
+	should_achrive_posts bool
+	should_show_media_in_comments_settings bool
+	show_media bool
+	show_media_preview bool
+	spoilers_enabled bool
+	submission_type string
+submit_link_label string
+	submit_text string
+	submit_text_html string
+	submit_text_label string
+	subreddit_type string
+	subscribers int
+	// suggested_comment_sort TODO: find type
+	title string
+	url string
+	user_can_flair_in_sr bool
+	user_flair_background_color ?string
+	user_flair_css_class ?string
+	user_flair_enabled_in_sr bool
+	user_flair_position ?string
+	// user_flair_richtext [] TODO: find type
+	user_flair_template_id ?string
+	user_flair_text ?string
+	user_flair_text_color ?string
+	user_flair_type string
+	user_has_favorited bool
+	user_is_banned bool
+	user_is_contributor bool
+	user_is_moderator bool
+	user_is_muted bool
+	user_is_subscriber bool
+	user_sr_flair_enabled bool
+	user_sr_theme_enabled bool
+	videostream_links_count int
+	whitelist_status string
+	wiki_enabled bool
+	wls int
+}
+
+struct Subreddit_CommentContributionSettings {
+	allowed_media_types []string
+}
+
+
+// about_user fetches a user from reddit, it pulls their /about.json and parses it into a V struct
+pub fn about_user(name string) !User {
 	mut resp := get('https://reddit.com/user/${name}/about.json') or {
 		return error('Failed to get user
 		(${name})\'s information (https://reddit.com/user/${name}/about.json).
--- veddit/about_test.v
+++ veddit/about_test.v
@@ -4,10 +4,10 @@ import net.http { get }
 import json
 
 // test_user this is such a bad test. but like, it works? ig.
-fn test_user() {
+fn test_about_user() {
 	mut resp := get('https://reddit.com/user/admin/about.json')!
 	our_user := json.decode(User, resp.body)!
 
-	mut fetched_suer := user("admin")!
+	mut fetched_suer := about_user("admin")!
 	assert our_user == fetched_suer
 }
