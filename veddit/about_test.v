69d1d5f1a5ee43df7eade0099339b2672876dc90
AceLikesGhosts <acelikesghosts@gmail.com>
2023-05-04 01:13:29 -0700

    FIX(tests): ... u/admin is not a reddit admin. uhoh! -> move to u/reddit

--- veddit/about_test.v
+++ veddit/about_test.v
@@ -5,7 +5,7 @@ import json
 
 // test_user this is such a bad test. but like, it works? ig.
 fn test_about_user() {
-	mut resp := get('https://reddit.com/user/admin/about.json')!
+	mut resp := get('https://reddit.com/user/reddit/about.json')!
 	our_user := json.decode(User, resp.body)!
 
 	mut fetched_suer := about_user('admin')!
