1a42d074a8a913f71d5ee49479968af1a61178df
AceLikesGhosts <acelikesghosts@gmail.com>
2023-05-04 01:17:03 -0700

    FIX(tests): fix spelling of `fetched_user` from `fetched_suer`

--- veddit/about_test.v
+++ veddit/about_test.v
@@ -8,19 +8,19 @@ fn test_about_user() {
 	mut resp := get('https://reddit.com/user/reddit/about.json')!
 	our_user := json.decode(User, resp.body)!
 
-	mut fetched_suer := about_user('admin')!
-	assert our_user == fetched_suer
+	mut fetched_user := about_user('admin')!
+	assert our_user == fetched_user
 
-	assert fetched_suer.kind == 't2' // t2 means its a user
+	assert fetched_user.kind == 't2' // t2 means its a user
 
-	assert fetched_suer.data.created_utc == 1134104400
-	assert fetched_suer.data.created == 1134104400
+	assert fetched_user.data.created_utc == 1134104400
+	assert fetched_user.data.created == 1134104400
 
-	assert fetched_suer.data.is_employee == true
-	assert fetched_suer.data.is_mod == true
-	assert fetched_suer.data.is_gold == true
+	assert fetched_user.data.is_employee == true
+	assert fetched_user.data.is_mod == true
+	assert fetched_user.data.is_gold == true
 
-	assert fetched_suer.data.accept_chats == false
-	assert fetched_suer.data.accept_pms == true
-	assert fetched_suer.data.accept_followers == true
+	assert fetched_user.data.accept_chats == false
+	assert fetched_user.data.accept_pms == true
+	assert fetched_user.data.accept_followers == true
 }
